3e99d99be7e3dd9c7c1b5c5cb11b60bd12d4a875
Vladimir Tomanovski <vladoohr@gmail.com>
2019-03-29 23:01:41 +0100

    Predict rest of the sentence - initial work

--- rnn.py
+++ rnn.py
@@ -24,27 +24,28 @@ import re
 
 path = 'nietzsche.txt'
 text = open(path).read().lower()
-# text = 'vladimir is a really big fan on bayern muchen'
+text = 'vladimir is a really big fan of bayern muchen.'
 print('corpus length:', len(text))
 
 text = text.replace('\n', ' ')
 
-# sentences = filter(None, re.split("[\.\?\!]", text))
-words = text.split()
-sentences = []
-SEQUENCE_LENGTH = 3
-next_words = []
+sentences = filter(None, re.split("[\.\?\!]", text))
+SEQUENCE_LENGTH = 10
+possible_sentences = []
+next_fragments = []
 
-for i in range(0, len(words)-SEQUENCE_LENGTH, 1):
-    sentences.append(words[i:i+SEQUENCE_LENGTH])
-    next_words.append(words[i+SEQUENCE_LENGTH])
-print('num training examples: %d ' % len(sentences))
+for i in range(0, len(sentences), 1):
+    words = sentences[i].split()
+    for j in range(1, len(words)):
+        possible_sentences.append(' '.join(words[0:j]))
+        next_fragments.append(' '.join(words[-(len(words)-j):]))
+print('num training examples: %d ' % len(possible_sentences))
 
-words = sorted(list(set(words)))
-word_indices = dict((w, i) for i, w in enumerate(words))
-indices_word = dict((i, w) for i, w in enumerate(words))
+fragments = sorted(list(set(possible_sentences)))
+fragment_indices = dict((f, i) for i, f in enumerate(fragments))
+indices_fragment = dict((i, f) for i, f in enumerate(fragments))
 
-print('unique words: %d' % len(words))
+print('unique fragments: %d' % len(fragments))
 
 # SEQUENCE_LENGTH = 40
 # step = 3
@@ -54,26 +55,29 @@ print('unique words: %d' % len(words))
 #     sentences.append(text[i: i + SEQUENCE_LENGTH])
 #     next_chars.append(text[i + SEQUENCE_LENGTH])
 
+print fragment_indices
 
-X = np.zeros((len(sentences), SEQUENCE_LENGTH, len(words)), dtype=np.bool)
-y = np.zeros((len(sentences), len(words)), dtype=np.bool)
+X = np.zeros((len(sentences), SEQUENCE_LENGTH, len(next_fragments)), dtype=np.bool)
+y = np.zeros((len(possible_sentences), len(next_fragments)), dtype=np.bool)
 for i, sentence in enumerate(sentences):
-    for t, word in enumerate(sentence):
-        X[i, t, word_indices[word]] = 1
-    y[i, word_indices[next_words[i]]] = 1
+    for t, f in enumerate(possible_sentences):
+        X[i, t, fragment_indices[f]] = 1
+    # y[i, fragment_indices[next_fragments[i]]] = 1
 
-model = Sequential()
-model.add(LSTM(128, input_shape=(SEQUENCE_LENGTH, len(words))))
-model.add(Dense(len(words)))
-model.add(Activation('softmax'))
+print X
 
-optimizer = RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-history = model.fit(X, y, validation_split=0.05, batch_size=128, epochs=50, shuffle=True).history
+# model = Sequential()
+# model.add(LSTM(128, input_shape=(SEQUENCE_LENGTH, len(words))))
+# model.add(Dense(len(words)))
+# model.add(Activation('softmax'))
 
-model.save('keras_model_words.h5')
-pickle.dump(history, open("history.p", "wb"))
-# model = load_model('keras_model_words.h5')
+# optimizer = RMSprop(lr=0.01)
+# model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+# history = model.fit(X, y, validation_split=0.05, batch_size=128, epochs=50, shuffle=True).history
+
+# model.save('keras_model.h5')
+# pickle.dump(history, open("history.p", "wb"))
+# model = load_model('keras_model.h5')
 
 def prepare_input(text):
     x = np.zeros((1, SEQUENCE_LENGTH, len(words)))
@@ -111,15 +115,13 @@ def predict_completions(text, n=3):
     next_indices = sample(preds, n)
     return [indices_word[idx] for idx in next_indices]
 
-quotes = [
-    'truth is a',
-    'for suspecting that',
-    'there not ground'
-]
+# quotes = [
+#     'One can never',
+# ]
 
-for q in quotes:
-    seq = q.lower()
-    print(seq)
-    print(predict_completions(seq, 5))
-    print('\n')
+# for q in quotes:
+#     seq = q.lower()
+#     print(seq)
+#     print(predict_completions(seq, 5))
+#     print('\n')
 
